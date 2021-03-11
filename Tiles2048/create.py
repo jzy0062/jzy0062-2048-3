1a5e605641bf387ad31fcdcb903ef1bace3f0f85
jzy0062 <jzy0062@auburn.edu>
2021-03-11 23:29:22 +0800

    Red Light |

--- Tiles2048/create.py
+++ Tiles2048/create.py
@@ -7,9 +7,17 @@ def _create(userParms):
 
 def generate_init_loc():
     """
-    generate 2 different place of initial two 2.
+    generate 2 different place of initial two '2'.
     :return: two integer belongs to [1, 15]
     """
     loc1 = random.randint(0, 15)
     loc2 = random.randint(0, loc1-1) if loc1 > 8 else random.randint(loc1 + 1, 15)
     return loc1, loc2
+
+
+def generate_init_grid():
+    """
+    generate initial grid string with 2 initial '2'
+    :return: string of grid containing two '2' in string
+    """
+    return ""
--- Tiles2048/test/createTest.py
+++ Tiles2048/test/createTest.py
@@ -3,8 +3,8 @@ import Tiles2048.create as create
 
 class CreateTest(unittest.TestCase):
 
-#  The following is included only for illustrative purposes.  It tests nothing
-#  useful.  Please delete and replace with your own test code.
+    #  The following is included only for illustrative purposes.  It tests nothing
+    #  useful.  Please delete and replace with your own test code.
     def test_create_HappyPathTest010(self):
         userParms = {'op': 'create', 'size': '4'}
         actualResult = create._create(userParms)
@@ -19,3 +19,18 @@ class CreateTest(unittest.TestCase):
             self.assertTrue(loc2 >= 0, "initial location out of range")
             self.assertTrue(loc1 <= 15, "initial location out of range")
             self.assertTrue(loc2 <= 15, "initial location out of range")
+
+    def test_init_grid_Should_valid(self):
+        test_times = 1000
+        for i in range(test_times):
+            grid = create.generate_init_grid()
+            self.assertEqual(16, len(grid), "initial grid invalid, len:" + str(grid))
+            two_count = 0
+            for ch in grid:
+                if ch == '2':
+                    two_count += 1
+                else:
+                    self.assertEqual('0', ch, "invalid character in grid :" + ch)
+
+
+
